608b8ac48d2d71bbbf9406fa22c1f82732958b09
Moreno Almeida <moreno.ferreira11@gmail.com>
2026-08-03 21:06:05 -0300

    calendario

--- src/pages/Calendery/index.tsx
+++ src/pages/Calendery/index.tsx
@@ -1,1 +1,9 @@
-//teste
+import React from "react";
+
+export default function Calendery() {
+    return (
+        <div className="w-full min-h-screen">
+            <h1>Calendário</h1>
+        </div>
+    );
+}
